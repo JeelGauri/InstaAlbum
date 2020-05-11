5d966fb6e2c205382a45dc00e21014658470a772
DivyaDakoria <64150456+DivyaDakoria@users.noreply.github.com>
2020-05-11 19:30:06 +0530

    user side

---
+++ InstaAlbum/Controllers/UserHomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstaAlbum.Controllers
+{
+    public class UserHomeController : Controller
+    {
+        // GET: UserHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Protfolio()
+        {
+            return View();
+        }
+        public ActionResult Category()
+        {
+            return View();
+        }
+        public ActionResult SubCategory()
+        {
+            return View();
+        }
+        public ActionResult CategoryWiseImage()
+        {
+            return View();
+        }
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+    }
+}
