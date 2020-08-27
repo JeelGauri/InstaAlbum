48dca04cd1e3c1bebe6f88bd8b4a1d369e33db61
JeelGauri <63703537+JeelGauri@users.noreply.github.com>
2020-08-27 18:54:17 +0530

    Model changes by jeel 6:54 pm

--- InstaAlbum/Models/InstaAlbumDB.Designer.cs
+++ InstaAlbum/Models/InstaAlbumDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\DIVYA DAKORIA\Source\Repos\InstaAlbum\InstaAlbum\Models\InstaAlbumDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jeelgauri\source\repos\ProjectInstaAlbum\InstaAlbum\Models\InstaAlbumDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
